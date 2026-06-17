6e87f8b8dd223f738a8b612d6b730ad4a279db78
Sr.Yamil <232170@unamba.edu.pe>
2026-06-17 14:22:46 -0500

    Eliminar todo el contenido del repositorio

--- src/app/features/pages/store/expiration/expiration.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-expiration',
-  imports: [],
-  templateUrl: './expiration.html',
-  styleUrl: './expiration.css',
-})
-export class Expiration {}
--- src/app/features/pages/store/kardex/kardex.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-kardex',
-  imports: [],
-  templateUrl: './kardex.html',
-  styleUrl: './kardex.css',
-})
-export class Kardex {}
