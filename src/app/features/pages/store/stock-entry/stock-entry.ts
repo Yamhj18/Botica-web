3ee4f2a93e22768569c26a601fd14cb10bd87060
Sr.Yamil <232170@unamba.edu.pe>
2026-06-17 14:22:14 -0500

    Eliminar todo el contenido del repositorio

--- src/app/features/pages/store/stock-entry/stock-entry.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-stock-entry',
-  imports: [],
-  templateUrl: './stock-entry.html',
-  styleUrl: './stock-entry.css',
-})
-export class StockEntry {}
--- src/main/java/com/epiis/apirfbvc/repository/RepositoryLaboratory.java
+++
@@ -1,9 +0,0 @@
-package com.epiis.apirfbvc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.epiis.apirfbvc.entity.EntityLaboratory;
-
-@Repository
-public interface RepositoryLaboratory extends JpaRepository<EntityLaboratory, String>{}
